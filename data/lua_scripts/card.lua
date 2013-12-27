229bbcbe264879c14dc10dff12a8490507a1b6a6
Korman <kormanwork@gmail.com>
2013-12-28 05:22:36 +0800

    添加卡牌类和卡牌管理类

---
+++ data/lua_scripts/card.lua
@@ -0,0 +1,23 @@
+--------------------------------------------------------------
+-- FileName: 	card.lua
+-- author:		郭浩, 2013/12/28
+-- purpose:		卡牌类（多实例）
+-- 注意：		这个类会有多个实例，注意用法！
+--------------------------------------------------------------
+
+card = {};
+local p = card;
+
+function p:new()	
+	o = {}
+	setmetatable( o, self );
+	self.__index = self;
+	o:ctor();
+	return o;
+end
+
+--构造函数
+function p:ctor()
+	self.m_pSprite = nil;
+	self.m_nNumber = 0;
+end
